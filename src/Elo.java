1ccc2fdfc74c059cc0806a59919430329ad06472
Maciej Wójcik <maciejwojcik15@gmail.com>
2019-02-14 20:34:04 +0100

    pierwszy commit

---
+++ src/Elo.java
@@ -0,0 +1,29 @@
+public class Elo {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+        System.out.println("Maciej Jest kurwa najelpszy");
+
+        int a = 2;
+        int b = 3;
+        int c = a + b;
+        byte liczba = 125;
+        String imie = "Maciej";
+        String nazwisko = "Wójcik";
+        System.out.println(imie + " " + nazwisko);
+        System.out.println(liczba);
+        System.out.println((double)a / b); // double oznacza liczby z przecinkiem
+        System.out.println(a++); // post inkrementacja
+        System.out.println(a);
+        System.out.println(++b); // pre inkrementacja
+        boolean isTrue = 4 == 4;
+        if (a == b || c > b);
+        System.out.println();
+
+    }
+
+
+
+}
+
